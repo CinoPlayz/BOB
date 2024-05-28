65721ef28c9a38b5fc107bff61b3f53bc1a923bc
CinoPlayz <90247617+CinoPlayz@users.noreply.github.com>
2024-05-28 12:19:11 +0200

    BOB-306 Added bar graph for delays by train

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,40 +1,46 @@
 import React, { useState, useEffect, useContext } from 'react';
 import TrainContext from '../TrainContext';
-import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryContainer, VictoryZoomContainer, VictoryBrushContainer, Background } from 'victory';
 
 function Stats() {
     const { fetchStats } = useContext(TrainContext);
     const [statsData, setStatsData] = useState([]);
     const [mostDelays, setMostDelays] = useState([]);
-    const [dataByFilter, setDataByFilter] = useState([{keys: "T", averageDelayByFilter: 1}]);
+    const [dataByFilter, setDataByFilter] = useState([{ index: 0, keys: "T", averageDelayByFilter: 1 }]);
+    const [zoomDomain, setZoomDomain] = useState({});
+    const [selectedDomain, setSelectedDomain] = useState({});
+
 
     function groupBy(list, keyGetter) {
         const map = new Map();
         list.forEach((item) => {
-             const key = keyGetter(item);
-             const collection = map.get(key);
-             if (!collection) {
-                 map.set(key, [item]);
-             } else {
-                 collection.push(item);
-             }
+            const key = keyGetter(item);
+            const collection = map.get(key);
+            if (!collection) {
+                map.set(key, [item]);
+            } else {
+                collection.push(item);
+            }
         });
         return map;
     }
-    
+
 
 
     const getStats = async () => {
         const data = await fetchStats();
         setStatsData(data);
-        
+
         const grouped = groupBy(statsData, stat => stat.route.trainNumber);
 
         let delayByFilter = []
         let delayLabelByFilter = []
 
         //console.log(Array.from(grouped));
-        grouped.forEach((values, keys) => {    
+        let countOfByFilter = 1;
+        let countOfMoreDelay = 0;
+        grouped.forEach((values, keys) => {
+            countOfByFilter += 1;
             let sum = 0;
             let count = 0;
             values.forEach((data) => {
@@ -44,19 +50,24 @@ function Stats() {
 
             let averageDelayByFilter = sum / count;
 
-            delayByFilter.push({keys, averageDelayByFilter})
+            if(averageDelayByFilter > 0){
+                countOfMoreDelay += 1;
+            }
+            delayByFilter.push({index: countOfByFilter, keys, averageDelayByFilter })
             delayLabelByFilter.push(keys)
             //console.log(values, keys);
         });
 
-        if(delayByFilter.length != 0){
+        if (delayByFilter.length != 0) {
             setDataByFilter(delayByFilter);
         }
 
-        
+
 
         console.log("Delay: ")
         console.log(delayByFilter);
+
+        console.log(countOfMoreDelay);
 
     };
 
@@ -71,15 +82,95 @@ function Stats() {
                     dataByFilter.map(data => <li key={data.keys}>{data.averageDelayByFilter}</li>)
                 }
             </ul>
-            <VictoryChart domainPadding={20}>
-                <VictoryAxis tickValues={dataByFilter.map(data => data.keys)}>
+            <VictoryChart containerComponent={
+                <VictoryZoomContainer zoomDomain={
+                    { x: [5, 35], y: [0, 25] }
+                }
+                    zoomDimension="x" />
+            }>
+                <VictoryAxis tickValues={dataByFilter.map(data => data.keys)} tickFormat={dataByFilter.map(data => data.keys)}>
 
                 </VictoryAxis>
                 <VictoryAxis dependentAxis>
-                <VictoryBar data={dataByFilter} x="keys"  y="averageDelayByFilter" />
                 </VictoryAxis>
+                <VictoryBar data={dataByFilter} x="keys" y="averageDelayByFilter" />
             </VictoryChart>
-           
+
+            <div>
+                <VictoryChart
+                    width={550}
+                    height={300}
+
+                    containerComponent={
+                        <VictoryZoomContainer responsive={false}
+                            zoomDimension="x"
+                            allowZoom={false}
+                            allowPan
+                            zoomDomain={zoomDomain}
+                            onZoomDomainChange={(domain, props) => setSelectedDomain(domain)}
+                        />
+                    }
+                >
+
+                    <VictoryAxis tickValues={dataByFilter.map(data => data.index)} tickFormat={dataByFilter.map(data => data.keys)}></VictoryAxis>
+
+                    <VictoryAxis dependentAxis>
+                    </VictoryAxis>
+
+                    <VictoryBar
+                        style={{
+                            data: { stroke: "tomato" }
+                        }}
+                        data={dataByFilter}
+                        x="index" y="averageDelayByFilter"
+                    />
+
+                </VictoryChart>
+
+                <VictoryChart
+                    width={550}
+                    height={40}
+                    scale={{ x: "index" }}
+                    domainPadding={{ x: [10, 10] }}
+                    padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
+
+                    containerComponent={
+                        <VictoryBrushContainer responsive={false}
+                            brushDimension="x"
+                            brushDomain={selectedDomain}
+                            allowDrag
+
+                            allowDraw
+                            brushStyle={{ stroke: "transparent", fill: "black", fillOpacity: 1 }}
+                            brushComponent={<rect rx="5" ry="5" width="5" />}
+                            onBrushDomainChange={(domain, props) => setZoomDomain(domain)}
+                        />
+                    }
+                    style={{
+                        background: { fill: "#c0c0c0" }
+                    }}
+                    backgroundComponent={<Background rx="5" ry="5" />}
+                >
+                    <VictoryAxis
+                        tickValues={dataByFilter.map(data => data.index)}
+                        style={{
+                            axis: { display: "none" },
+                            labels: { display: "none" },
+                            ticks: { display: "none" }
+                        }}
+                        tickFormat={(x) => null}
+                    />
+                    
+                    <VictoryBar
+                        style={{
+                            data: { stroke: "transparent", fill: 'transparent' }
+                        }}
+                        data={dataByFilter}
+                        x="index" y="averageDelayByFilter"
+                    />
+                </VictoryChart>
+            </div>
+
         </div>
     );
 }
