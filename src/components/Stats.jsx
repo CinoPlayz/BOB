8ff5965e951cf24e9be5fb42fb3971a268312d12
CinoPlayz <90247617+CinoPlayz@users.noreply.github.com>
2024-05-28 17:01:33 +0200

    BOB-306 Added sorting

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect, useContext } from 'react';
 import TrainContext from '../TrainContext';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryContainer, VictoryZoomContainer, VictoryBrushContainer, Background, VictoryTheme, VictoryTooltip } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryZoomContainer, VictoryBrushContainer, Background, VictoryTheme, VictoryTooltip, VictoryPie } from 'victory';
+import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+
+const orderByOptions = [{ id: 1, name: "Unordered" }, { id: 2, name: "Asc" }, { id: 3, name: "Desc" }]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
 
 function Stats() {
     const { fetchStats } = useContext(TrainContext);
     const [statsData, setStatsData] = useState([]);
-    const [dataByFilter, setDataByFilter] = useState([{ index: 0, keys: "T", averageDelayByFilter: 1 }]);
-    const [zoomDomain, setZoomDomain] = useState({x: [ 1.645427709190672, 9.049852400548696 ], y: [ 0, 25.1 ]});
+    const [dataByFilter, setDataByFilter] = useState([{ index: 0, trainName: "T", averageDelayByFilter: 1 }]);
+    const [dataByFilterPie, setDataByFilterPie] = useState([{ index: 0, trainNames: [], averageDelayByFilter: 1 }]);
+    const [zoomDomain, setZoomDomain] = useState({ x: [1.645427709190672, 9.049852400548696], y: [0, 25.1] });
     const [selectedDomain, setSelectedDomain] = useState({});
-    const [orderBy, setOrderBy] = useState("unordered");
+    const [orderBy, setOrderBy] = useState(orderByOptions[0]);
+
+
 
 
     function groupBy(list, keyGetter) {
@@ -50,25 +61,141 @@ function Stats() {
 
             let trainName = values[0].route.trainType + " " + values[0].route.trainNumber
 
-            delayByFilter.push({ index: countOfByFilter, keys: trainName, averageDelayByFilter })
+            delayByFilter.push({ index: countOfByFilter, trainName: trainName, averageDelayByFilter })
         });
 
         if (delayByFilter.length != 0) {
             setDataByFilter(delayByFilter);
         }
+
+
+        const groupedByDelay = groupBy(delayByFilter, stat => Math.round(stat.averageDelayByFilter));
+        let delayByFilterPie = []
+        let countOfByFilterPie = 0;
+
+        //console.log(Array.from(groupedByDelay));
+
+        groupedByDelay.forEach((values, keys) => {
+            countOfByFilterPie += 1
+
+            let trainNames = []
+            let countOfTrainsByDelay = 0;
+
+            values.forEach((data) => {
+                trainNames.push(data.trainName);
+                countOfTrainsByDelay += 1;
+            });
+
+            let averageDelay = Math.round(values[0].averageDelayByFilter)
+
+            delayByFilterPie.push({ index: countOfByFilterPie, trainNames: trainNames, averageDelayByFilter: averageDelay, countOfTrainsByDelay: countOfTrainsByDelay, label: "Zamuda: " + averageDelay + "\nSt: " + countOfTrainsByDelay })
+        });
+
+        if (delayByFilterPie.length != 0) {
+            setDataByFilterPie(delayByFilterPie);
+        }
+
+        console.log(delayByFilterPie)
+
+
     };
 
     useEffect(() => {
         getStats()
     }, []);
 
+    useEffect(() => {
+        let orderDelay = dataByFilter.slice(0);
+        let orderDelayPie = dataByFilterPie.slice(0);
+
+        switch (orderBy.id){
+            case 2 : {
+                orderDelay.sort(function(a,b) {
+                    return a.averageDelayByFilter - b.averageDelayByFilter;
+                });
+                orderDelayPie.sort(function(a,b) {
+                    return a.averageDelayByFilter - b.averageDelayByFilter;
+                });
+            }
+            case 3: {
+                orderDelay.sort(function(a,b) {
+                    return b.averageDelayByFilter - a.averageDelayByFilter;
+                });
+                orderDelayPie.sort(function(a,b) {
+                    return b.averageDelayByFilter - a.averageDelayByFilter;
+                });
+            }
+            default: {
+
+            }
+        }
+
+        setDataByFilter(orderDelay);        
+        setDataByFilterPie(orderDelayPie);
+        
+
+    }, [orderBy])
+    
+
     return (
         <div>
-            <ul>
-                {
-                    dataByFilter.map(data => <li key={data.keys}>{data.averageDelayByFilter}</li>)
-                }
-            </ul>
+
+            <Listbox value={orderBy} onChange={setOrderBy}>
+                {({ open }) => (
+                    <>
+                        <Label className="block text-sm font-medium leading-6 text-gray-900">Order By</Label>
+
+                        <div className="relative mt-2">
+                            <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <span className="block truncate">{orderBy.name}</span>
+                                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                    <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                </span>
+                            </ListboxButton>
+
+                            <Transition show={open} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+                                <ListboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                    {orderByOptions.map((order) => (
+                                        <ListboxOption
+                                            key={order.id}
+                                            className={({ focus }) =>
+                                                classNames(
+                                                    focus ? 'bg-indigo-600 text-white' : '',
+                                                    !focus ? 'text-gray-900' : '',
+                                                    'relative cursor-default select-none py-2 pl-3 pr-9'
+                                                )
+                                            }
+                                            value={order}
+                                        >
+                                            {({ selected, focus }) => (
+                                                <>
+                                                    <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                                        {order.name}
+                                                    </span>
+
+                                                    {selected ? (
+                                                        <span
+                                                            className={classNames(
+                                                                focus ? 'text-white' : 'text-indigo-600',
+                                                                'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                            )}
+                                                        >
+                                                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                        </span>
+                                                    ) : null}
+                                                </>
+                                            )}
+                                        </ListboxOption>
+                                    ))}
+                                </ListboxOptions>
+                            </Transition>
+                        </div>
+                    </>
+                )}
+            </Listbox>
+
+
+
 
             <div>
                 <VictoryChart
@@ -87,7 +214,7 @@ function Stats() {
                     }
                 >
 
-                    <VictoryAxis tickValues={dataByFilter.map(data => data.index)} tickFormat={dataByFilter.map(data => data.keys)}></VictoryAxis>
+                    <VictoryAxis tickValues={dataByFilter.map(data => data.index)} tickFormat={dataByFilter.map(data => data.trainName)}></VictoryAxis>
 
                     <VictoryAxis dependentAxis>
                     </VictoryAxis>
@@ -100,7 +227,7 @@ function Stats() {
                         data={dataByFilter}
                         x="index" y="averageDelayByFilter"
                         labels={dataByFilter.map(data => Math.round(data.averageDelayByFilter))}
-                        labelComponent={<VictoryTooltip/>}
+                        labelComponent={<VictoryTooltip />}
                     />
 
                 </VictoryChart>
@@ -147,7 +274,24 @@ function Stats() {
                         x="index" y="averageDelayByFilter"
                     />
                 </VictoryChart>
+
+                <VictoryPie
+                    theme={VictoryTheme.material}
+                    data={dataByFilterPie}
+                    innerRadius={100}
+                    labelComponent={<VictoryTooltip />}
+                    x="averageDelayByFilter" y="countOfTrainsByDelay">
+
+
+                </VictoryPie>
+
             </div>
+
+            <ul>
+                {
+                    dataByFilter.map(data => <li key={data.trainName}>{data.averageDelayByFilter}</li>)
+                }
+            </ul>
 
         </div>
     );
