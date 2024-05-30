b04d09e0f57af06fd4d89e254b182b3c5a2992e5
markokurnik <marko@kurnik.si>
2024-05-30 17:47:29 +0200

    Refactor
    BOB-293: CustomDropdownMenu

--- src/main/kotlin/gui/Shared.kt
+++ src/main/kotlin/gui/Shared.kt
@@ -159,3 +159,92 @@ fun parseShortTime(timeStr: String): Pair<String, String> {
 
     return Pair(hours, minutes)
 }
+
+@Composable
+fun CustomDropdownMenu(
+    label: String,
+    options: List<String>,
+    initialSelection: String? = null,
+    onSelectionChange: (String) -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    //var selectedOption by remember { mutableStateOf("") }
+    var selectedOption by remember { mutableStateOf(initialSelection ?: "") }
+
+    Box(modifier = Modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = selectedOption,
+            onValueChange = { }, // Disable text editing
+            readOnly = true,
+            label = { Text(label) },
+            trailingIcon = {
+                IconButton(
+                    onClick = { expanded = !expanded },
+                ) {
+                    Icon(Icons.Default.ArrowDownward, contentDescription = "Dropdown Menu")
+                }
+            },
+            modifier = Modifier
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(onClick = {
+                    selectedOption = option
+                    onSelectionChange(option)
+                    expanded = false
+                }) {
+                    Text(text = option)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomDropdownMenuInt(
+    label: String,
+    options: List<Int>,
+    initialSelection: Int? = null,
+    onSelectionChange: (Int) -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOption by remember { mutableStateOf(initialSelection ?: 0) }
+
+    Box(modifier = Modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = selectedOption.toString(),
+            onValueChange = { }, // Disable text editing
+            readOnly = true,
+            label = { Text(label) },
+            trailingIcon = {
+                IconButton(
+                    onClick = { expanded = !expanded },
+                ) {
+                    Icon(Icons.Default.ArrowDownward, contentDescription = "Dropdown Menu")
+                }
+            },
+            modifier = Modifier
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(onClick = {
+                    selectedOption = option
+                    onSelectionChange(option)
+                    expanded = false
+                }) {
+                    Text(text = option.toString())
+                }
+            }
+        }
+    }
+}
