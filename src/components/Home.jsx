c392a31f13d70ebc0f79964750793f9d67c93ea7
markokurnik <marko@kurnik.si>
2024-06-04 17:00:56 +0200

    Homepage, register
    BOB-424
    BOB-417
    BOB-419
    BOB-420

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,64 @@
 import React from 'react';
 import FrontPhoto from '../assets/front.png';
 import '../style/Home.css';
+import { Link, useLocation } from 'react-router-dom';
+import { Fragment, useState, useContext } from 'react';
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { UserContext } from "../UserContext.jsx";
+
 
 function Home() {
+    const isActive = (path) => {
+        return location.pathname === path ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700';
+    };
+
+    const userContext = useContext(UserContext);
+
     return (
         <div className="home-container">
             <div className="left-side">
                 <img src={FrontPhoto} alt="Front" className="overlapping-image" />
             </div>
             <div className="right-side">
-                <p>BandOfBytes</p>
+                <h1>Welcome to advanced railroad system tracking service.</h1>
+                <p>
+                    Explore our{' '}
+                    <Link to="/map" className={`font-bold ${isActive('/map')}`}>
+                        Map
+                    </Link>
+                </p>
+                <UserContext.Consumer>
+                    {context => (
+                        context.token ?
+                            <>
+                                <p>
+                                    Save personal{' '}
+                                    <Link to="/historyMap" className={`font-bold ${isActive('/historyMap')}`}>
+                                        Travel History
+                                    </Link>
+                                    .
+                                </p>
+                                <p>
+                                    Explore{' '}
+                                    <Link to="/stats" className={`font-bold ${isActive('/stats')}`}>
+                                        Advanced Stats
+                                    </Link>
+                                    .
+                                </p>
+                            </>
+                            :
+                            <>
+                                <p>
+                                    <Link to="/register" className={`font-bold ${isActive('/register')}`}>
+                                        Become a member
+                                    </Link>
+                                    {' '}for full functionality.
+                                </p>
+                            </>
+                    )}
+                </UserContext.Consumer>
             </div>
-            {/* <Footer /> */} {/* Included globally in App.jsx */}
         </div>
     );
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,12 +6,19 @@ import trainIcon from '../assets/train_icon.png';
 function Register() {
     const [username, setUsername] = useState([]);
     const [password, setPassword] = useState([]);
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [email, setEmail] = useState([]);
     const [error, setError] = useState([]);
     const userContext = useContext(UserContext);
 
     const handleRegister = async (e) => {
         e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError('Passwords do not match!');
+            return;
+        }
+
         const res = await fetch("http://localhost:3001/users", {
             method: "POST",
             credentials: "include",
@@ -61,7 +68,7 @@ function Register() {
                                 autoComplete="email"
                                 required
                                 className="pl-2 block  w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                placeholder=""
+                                placeholder={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
@@ -99,6 +106,24 @@ function Register() {
                                 className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="confirmPassword" className="font-bold block text-sm font-medium leading-6 text-gray-900">
+                            Repeat Password
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="password"
+                                name="confirmPassword"
+                                id="confirmPassword"
+                                autoComplete="confirm password"
+                                required
+                                className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
                             />
                         </div>
                     </div>
