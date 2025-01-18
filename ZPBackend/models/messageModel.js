0f65cd35a267636497456db8ba675e44d52594aa
Cino <90247617+CinoPlayz@users.noreply.github.com>
2025-01-18 17:01:54 +0100

    Created messageModel BOB-498 BOB-555 BOB-564

---
+++ ZPBackend/models/messageModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    timeOfMessage: {
+        type: Date,
+        required: true
+    },
+    postedByUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        enum: ['miscellaneous', 'extreme'],
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('message', messageSchema);
